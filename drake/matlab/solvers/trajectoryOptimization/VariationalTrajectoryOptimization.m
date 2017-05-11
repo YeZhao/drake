cd46b530936a1479ae6b297f8126c19c30f09b63
Zac Manchester <zmanchester@g.harvard.edu>
2017-05-11 17:15:58 -0400

    Implemented setupVariables

--- drake/matlab/solvers/trajectoryOptimization/VariationalTrajectoryOptimization.m
+++ drake/matlab/solvers/trajectoryOptimization/VariationalTrajectoryOptimization.m
@@ -1,14 +1,16 @@
 classdef VariationalTrajectoryOptimization < DirectTrajectoryOptimization
     
     properties
-        Nc %Number of contact points
-        Nd %Number of basis vectors in polyhedral friction cone
+        nC %Number of contact points
+        nD %Number of basis vectors in polyhedral friction cone
+        twoD %Is the plant a PlanarRigidBodyManipulator?
         IntegrationMethod %Midpoint or Simpson's rule for integration
+        l_inds; %Indices of all contact-related variables
     end
     
     properties (Constant)
-        MIDPOINT = 1;
-        SIMPSON = 2;
+        MIDPOINT = 2;
+        SIMPSON = 3;
     end
     
     methods
@@ -22,14 +24,57 @@ classdef VariationalTrajectoryOptimization < DirectTrajectoryOptimization
                 options.integration_method = VariationalTrajectoryOptimization.MIDPOINT;
             end
             
+            typecheck(plant,'RigidBodyManipulator');
+            if isa(plant,'PlanarRigidBodyManipulator')
+                obj.twoD = true;
+            end
+            
             obj = obj@DirectTrajectoryOptimization(plant,N,duration,options);
         end
         
         function obj = setupVariables(obj, N)
             switch obj.options.integration_method
                 case VariationalTrajectoryOptimization.MIDPOINT
+                    
                     obj.IntegrationMethod = VariationalTrajectoryOptimization.MIDPOINT;
                     
+                    [phi,~,d] = obj.plant.contactConstraints(getZeroConfiguration(obj.plant), false, obj.options.active_collision_options);
+                    
+                    Nh = N-1;
+                    Nq = obj.plant.getNumPositions();
+                    nU = obj.plant.getNumInputs();
+                    nC = length(phi);
+                    nD = 2*length(d);
+                    nL = (3+2*nD)*nC; %Includes contact forces, contact distance, tangential speed, and friction cone
+                    
+                    obj.N = N;
+                    obj.nC = nC;
+                    obj.nD = nD;
+                    
+                    num_vars = Nh + N*Nq + (N-1)*nU + (N-1)*nL;
+                    
+                    obj.h_inds = (1:Nh)';
+                    obj.x_inds = reshape(Nh + (1:(N*Nq)), Nq, N);
+                    obj.u_inds = reshape(Nh+N*Nq + (1:N*nU), nU, N);
+                    obj.l_inds = reshape(Nh+N*Nq+(N-1)*nU + (1:((N-1)*nL)), nL, N);
+                    
+                    x_names = cell(num_vars,1);
+                    for i = 1:N
+                        if(i<N)
+                            x_names{i} = sprintf('h[%d]',i);
+                            for j = 1:nU
+                                x_names{nH+nX*N+(i-1)*nU+j} = sprintf('u%d[%d]',j,i);
+                            end
+                            for j = 1:nL
+                                x_names{nH+nX*N+nU*(N-1)+(i-1)*nL+j} = sprintf('l%d[%d]',j,i);
+                            end
+                        end
+                        for j = 1:nX
+                            x_names{nH+(i-1)*nX+j}=sprintf('x%d[%d]',j,i);
+                        end
+                    end
+                    
+                    obj = obj.addDecisionVariable(num_vars,x_names);
                     
                 case VariationalTrajectoryOptimization.SIMPSON
                     obj.IntegrationMethod = VariationalTrajectoryOptimization.SIMPSON;
@@ -41,9 +86,9 @@ classdef VariationalTrajectoryOptimization < DirectTrajectoryOptimization
         function obj = addDynamicConstraints(obj)
             Nq = obj.plant.getNumPositions();
             Nv = obj.plant.getNumVelocities;
-            Nu = obj.plant.getNumInputs();
-            Nc = obj.Nc;
-            Nd = obj.Nd;
+            nU = obj.plant.getNumInputs();
+            nC = obj.nC;
+            nD = obj.nD;
             N = obj.N;
             assert(Nq == Nv) % not quite ready for the alternative
             
@@ -74,7 +119,7 @@ classdef VariationalTrajectoryOptimization < DirectTrajectoryOptimization
         
         function [f,df] = midpoint_dynamics_constraint_fun(obj,h,q0,q1,u,lambda)
             Nq = obj.plant.getNumPositions;
-            Nu = obj.plant.getNumInputs;
+            nU = obj.plant.getNumInputs;
             
         end
         
